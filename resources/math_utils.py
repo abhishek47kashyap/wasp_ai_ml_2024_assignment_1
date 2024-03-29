0bfe2be71caeb62912cb4e75d99fe8291bbe54d9
Abhishek Kashyap <abhishek.kashyap@oru.se>
2024-03-29 17:59:53 +0100

    implemented point_falls_between_two_points()

--- resources/math_utils.py
+++ resources/math_utils.py
@@ -14,12 +14,7 @@ def distance_from_point_to_line_between_two_points(endpoint_a: EntityPosition, e
         shortest_distance = euclidean_distance(some_point, endpoint_a)
         return [shortest_distance, endpoint_a]
 
-    # get coefficients A, B, C for equation of a line where Ax + By + C = 0 (https://stackoverflow.com/a/13242831/6010333)
-    x1, y1 = endpoint_a.x, endpoint_a.y
-    x2, y2 = endpoint_b.x, endpoint_b.y
-    a = y1 - y2
-    b = x2 - x1
-    c = (x1 - x2) * y1 + (y2 - y1) * x1
+    [a, b, c] = get_equation_coeff_of_line_from_two_points(endpoint_a, endpoint_b)
 
     # distance and point on the line (https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line#Line_defined_by_an_equation)
     x0, y0 = some_point.x, some_point.y
@@ -35,7 +30,46 @@ def distance_from_point_to_line_between_two_points(endpoint_a: EntityPosition, e
     )
     return [shortest_distance, nearest_point]
 
-def point_falls_between_two_points(endpoint_a: EntityPosition, endpoint_b: EntityPosition, some_point: EntityPosition) -> bool:
-    ...
+def get_equation_coeff_of_line_from_two_points(point_a: EntityPosition, point_b: EntityPosition) -> list[int, int, int]:
+    """
+        Get coefficients A, B, C for equation of a line where Ax + By + C = 0 (https://stackoverflow.com/a/13242831/6010333)
+    """
+    x1, y1 = point_a.x, point_a.y
+    x2, y2 = point_b.x, point_b.y
+    a = y1 - y2
+    b = x2 - x1
+    c = (x1 - x2) * y1 + (y2 - y1) * x1
+    return [a, b, c]
 
-    # see https://math.stackexchange.com/questions/1915322/determine-if-a-point-lies-between-two-parallel-lines
+def point_falls_between_two_points(endpoint_a: EntityPosition, endpoint_b: EntityPosition, some_point: EntityPosition) -> bool:
+    """
+        Returns whether a point falls in the region between two points.
+        See https://math.stackexchange.com/questions/1915322/determine-if-a-point-lies-between-two-parallel-lines
+    """
+    [a, b, _] = get_equation_coeff_of_line_from_two_points(endpoint_a, endpoint_b)
+
+    """
+        Working out the math to obtain equation of line passing through some_point and being perpendicular to the line
+        connecting endpoint_a and endpoint_b, the new coefficients are:
+            A' = -B
+            B' = A
+            C' = (B * some_point.x) - (A * some_point.y)
+    """
+
+    def get_signed_distance(A, B, C, X, Y) -> float:
+        numerator = (A * X) + (B * Y) + C
+        denominator = (A**2 + B**2) ** 0.5
+        return (numerator / denominator)
+
+    # normal line
+    a_normal = -b
+    b_normal = a
+
+    c_endpoint_a = (b * endpoint_a.x) - (a * endpoint_a.y)
+    c_endpoint_b = (b * endpoint_b.x) - (a * endpoint_b.y)
+
+    dist1 = get_signed_distance(a_normal, b_normal, c_endpoint_a, some_point.x, some_point.y)
+    dist2 = get_signed_distance(a_normal, b_normal, c_endpoint_b, some_point.x, some_point.y)
+
+    falls_between = (dist1 <= 0 and dist2 >= 0) or (dist1 >= 0 and dist2 <= 0)  # dist1 and dist2 should have opposite sites
+    return falls_between
--- tests/math_utils.py
+++ tests/math_utils.py
@@ -32,6 +32,22 @@ class TestMathUtils(unittest.TestCase):
         expected_result = [3.0, EntityPosition(x = 3.0, y = 30.0)]
         self.assertEqual(result, expected_result, f"Expected {expected_result} but got {result}")
 
+    def test_1_point_falls_between_two_points(self):
+        endpoint_a = EntityPosition(x = 0, y = -3)
+        endpoint_b = EntityPosition(x = 0, y = 3)
+        some_point = EntityPosition(x = 0, y = 30)
+        result = math_utils.point_falls_between_two_points(endpoint_a, endpoint_b, some_point)
+        expected_result = False
+        self.assertEqual(result, expected_result, f"y = 30 cannot possibly be between y = [-3, 3] when x = 0")
+
+    def test_2_point_falls_between_two_points(self):
+        endpoint_a = EntityPosition(x = -45, y = 3)
+        endpoint_b = EntityPosition(x = 45, y = -3)
+        some_point = EntityPosition(x = 0, y = 0)
+        result = math_utils.point_falls_between_two_points(endpoint_a, endpoint_b, some_point)
+        expected_result = True
+        self.assertEqual(result, expected_result, f"Origin is between two points (x = -45, y = 3) and (x = 45, y = -3)")
+
 
 if __name__ == "__main__":
     unittest.main()
